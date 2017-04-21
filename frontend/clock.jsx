a79d2f73204c01e53c9104b440b46be822487dca
DanielLChang <daniellawrencechang@gmail.com>
2017-04-20 19:04:50 -0700

    Add greeting to clock

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -6,6 +6,7 @@ export default class Clock extends React.Component {
     super();
     this.state = {
       time: new Date(),
+      greeting: '',
     };
 
     this.tick = this.tick.bind(this);
@@ -16,13 +17,36 @@ export default class Clock extends React.Component {
   }
 
   tick() {
+    const hour = this.state.time.getHours();
+
+    if (hour >= 0 && hour < 12) {
+      this.setState({greeting: 'Good Morning'});
+    } else if (hour >= 12 && hour < 18) {
+      this.setState({greeting: 'Good Afternoon'});
+    } else {
+      this.setState({greeting: 'Good Evening'});
+    }
+    
     this.setState({time: new Date()});
+  }
+
+  greeting() {
+    const hour = this.state.time.getHours();
+
+    if (hour >= 0 && hour < 12) {
+      this.setState({greeting: 'Good Morning'});
+    } else if (hour >= 12 && hour < 18) {
+      this.setState({greeting: 'Good Afternoon'});
+    } else {
+      this.setState({greeting: 'Good Evening'});
+    }
   }
 
   render() {
     return (
       <div className="clock">
-        {this.state.time.toLocaleTimeString('en-US')}
+        <h1>{this.state.time.toLocaleTimeString('en-US')}</h1>
+        <h2>{this.state.greeting}</h2>
       </div>
     );
   }
