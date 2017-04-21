0222430f31b13683d282a89a537fc4ea199987ae
DanielLChang <daniellawrencechang@gmail.com>
2017-04-21 00:02:00 -0700

    gh-pages

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -26,20 +26,8 @@ export default class Clock extends React.Component {
     } else {
       this.setState({greeting: 'Good Evening'});
     }
-    
+
     this.setState({time: new Date()});
-  }
-
-  greeting() {
-    const hour = this.state.time.getHours();
-
-    if (hour >= 0 && hour < 12) {
-      this.setState({greeting: 'Good Morning'});
-    } else if (hour >= 12 && hour < 18) {
-      this.setState({greeting: 'Good Afternoon'});
-    } else {
-      this.setState({greeting: 'Good Evening'});
-    }
   }
 
   render() {
