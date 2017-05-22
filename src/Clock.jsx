ddb500f698c523003334f5ad8bf0d8ac33976e49
DanielLChang <daniellawrencechang@gmail.com>
2017-05-21 21:57:29 -0700

    Clock component

---
+++ src/Clock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Clock extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      time: '',
+    };
+
+    this.updateClock = this.updateClock.bind(this);
+  }
+
+  componentWillMount() {
+    this.updateClock();
+    setInterval(this.updateClock, 1000);
+  }
+
+  updateClock() {
+    const time = new Date;
+    this.setState({time: time.toLocaleTimeString('en-us')});
+  }
+
+  componentWillUnmount() {
+    clearInterval();
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.time}
+      </div>
+    );
+  }
+}
