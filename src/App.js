7e57daeb567f2b6ebcb4b5e8421d83b83e9bdd3a
DanielLChang <daniellawrencechang@gmail.com>
2017-05-23 20:21:47 -0700

    Add redux

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Clock from './Clock.jsx';
+import Todo from './Todo.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Clock />
+        <Todo />
       </div>
     );
   }
