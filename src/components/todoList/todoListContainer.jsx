3f18f5378b249df65402ac728e3726e12de5e0a7
DanielLChang <daniellawrencechang@gmail.com>
2017-05-25 20:28:55 -0700

    allTodo selector

---
+++ src/components/todoList/todoListContainer.jsx
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import todoList from './todoList.jsx';
+
+import { receiveTodos, receiveTodo } from '../../actions/todoActions.js';
+import { allTodos } from '../../reducers/selectors';
+
+const mapStateToProps = (state) => ({
+});
