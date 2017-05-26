1549ea83cb6147bfeb341abc49af6f9a176dac1a
DanielLChang <daniellawrencechang@gmail.com>
2017-05-25 20:42:02 -0700

    Todolist container

--- src/components/todoList/todoListContainer.jsx
+++ src/components/todoList/todoListContainer.jsx
@@ -5,4 +5,16 @@ import { receiveTodos, receiveTodo } from '../../actions/todoActions.js';
 import { allTodos } from '../../reducers/selectors';
 
 const mapStateToProps = (state) => ({
+  todos: allTodos(state),
+  state
 });
+
+const mapDispatchToProps = (dispatch) => ({
+  receiveTodos: () => dispatch(receiveTodos()),
+  receiveTodo: (todo) => dispatch(receiveTodo(todo))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(todoList);
