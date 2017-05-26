24515583cb5021acd5209218716706687681a152
DanielLChang <daniellawrencechang@gmail.com>
2017-05-25 20:39:21 -0700

    Todo reducer

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -3,6 +3,8 @@ import {
   RECEIVE_TODO,
 } from '../actions/todoActions.js';
 
+import merge from 'lodash/merge';
+
 const todosReducer = (state = {}, action) => {
   Object.freeze(state);
   let newState;
@@ -14,9 +16,10 @@ const todosReducer = (state = {}, action) => {
         const todo = action.todos[i];
         newState[todo.id] = todo;
       }
-
       return newState;
     case RECEIVE_TODO:
+      const newTodo = {[action.todo.id]: action.todo};
+      return merge({}, state, newTodo);
     default:
       return state;
   }
